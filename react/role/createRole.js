8790bc6f12c516ff0b1fcb0a5908f8c181d85a1b
DingZhicheng <954411071@qq.com>
2019-07-20 10:56:59 +0800

    增加校验，调整创建页面布局

--- react/role/createRole.js
+++ react/role/createRole.js
@@ -1,24 +1,70 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
-import { Button, Form ,Input,Select,SelectBox,Menu,Dropdown ,Icon } from 'choerodon-ui';
-import { Action, Content, Header, Page } from '@choerodon/boot';
+import { Button, Form ,Input,Select,Row,SelectBox,Menu,Dropdown ,Icon } from 'choerodon-ui';
+import { Action, Content, Header, Page} from '@choerodon/boot';
 import Store from './tableDemo/stores/Store';
 const { Option } = Select;
-
+const FormItem = Form.Item;
 @observer
 class CreateRole extends Component {
+
+
     renderInput(){
+      const{getFieldDecorator}= this.props.form;
         return(
-          <Form labelLayout="float" columns={2} >
-          <Input colSpan={3} addonBefore='/amdin/role' label="角色编码" name="code" required placeholder="请输入角色编码" />
-          <Input colSpan={3} label="角色名称" name="name" required placeholder="请输入角色名称" />
+      <Form  layout="vertical" >
+          <FormItem style={{display:"inline-block"}}
+          lable='请输入角色编码'
+          >
+          {getFieldDecorator('code', {
+            rules: 
+            [{ 
+              required: true, 
+              pattern:/^[a-zA-Z][A-Za-z0-9_\/\-]*$/,
+              message: '编码必须以字母开头，只能输入字母，数字，_，-，/' 
+            }],
+          })(
+            <Input 
+            prefix='/amdin/role'
+            label="角色编码" 
+            name="code"
+            placeholder="请输入角色编码"
+            size="large"
+            
+            />
+          )}
+            
+          
+        </FormItem>
+        <FormItem style={{display:"inline-block"}}>
+          {getFieldDecorator('name', {
+            rules:
+             [{ 
+              required: true, 
+              message: '请输入角色名称' 
+            }],
+          })(
+            <Input 
+            label="角色名称" 
+            name="name" 
+            placeholder="请输入角色名称"
+            size="large"
+            
+            />
+          )}
+            
+        </FormItem>
+        <FormItem >
           <Select label="" name="角色标签" required>
               <Option value="M">role</Option>
           </Select>
+      </FormItem>
+    
       </Form>
         );
     }
     renderLevel(text) {
+      
         const LEVEL_MAP = {
           organization: '组织',
           project: '项目',
@@ -28,12 +74,11 @@ class CreateRole extends Component {
     render() {
         return (
           <Page className="choerodon-role">
-            <Button  icon="playlist_add" onClick={this.handleCreate}>返回</Button>
-
-            <Header title={`创建${this.renderLevel(Store.level)}角色`}>
-             
-    
+            <div style={{lineHeight: "39px"}}>
+            <Header backPath="/index/role" title={`创建${this.renderLevel(Store.level)}角色`} >
+            
             </Header>
+            </div>
             <Content>
               {this.renderInput()}
             </Content>
@@ -42,6 +87,7 @@ class CreateRole extends Component {
       }
 }
 
+CreateRole = Form.create({})(CreateRole);
 
 
 
